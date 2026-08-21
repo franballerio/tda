5b69dd23ffe877842ae5ad0fbfe24d273a8a185e
franballerio <fballerio@dc.uba.ar>
2026-08-20 21:10:05 -0300

    feat(optional): separe la solucion real de la solucion que no estoy seguro si es valida

---
+++ tp0/tp(optional).py
@@ -0,0 +1,52 @@
+import time
+
+def erato2(n: int) -> list[bool]:
+
+    """Returns a list of all prime numbers up to n."""
+    if n < 2:
+        return []
+
+    # Initialize a boolean list tracking the prime status of numbers 0 to n
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime
+
+    # Only iterate up to the square root of n
+    for p in range(2, int(n**0.5) + 1):
+        if is_prime[p]:
+            # Mark all multiples of p starting from p*p as false
+            for i in range(p * p, n + 1, p):
+                is_prime[i] = False
+
+    # Filter out and return the indices that remain True
+    # return [num for num, prime in enumerate(is_prime) if prime]
+    return is_prime
+
+def esprimoV4(n: int, primes: list[bool]):
+    return primes[n]
+
+
+def prog():
+    t1 = time.time()
+    primes = erato2(int(1000000))
+    for i in range(11,1000000):
+        if esprimoV4(i, primes) and esprimoV4(i+2, primes) and esprimoV4(i+6, primes) and esprimoV4(i+8, primes):
+            print (i, i+2, i+6,i+8)
+    t2 = time.time()
+    times = t2 - t1
+    print(times)
+    return times
+
+def prog1():
+    t1 = time.time()
+    primes = erato2(int(1000000))
+    for i in range(11,1000000, 10):
+        if esprimoV4(i, primes) and esprimoV4(i+2, primes) and esprimoV4(i+6, primes) and esprimoV4(i+8, primes):
+            print (i, i+2, i+6,i+8)
+    t2 = time.time()
+    times = t2 - t1
+    print(times)
+    return times
+
+p = prog()
+print('-------------------------')
+p1 = prog1()
--- tp0/tp.py
+++ tp0/tp.py
@@ -19,27 +19,6 @@ def erato(n: int) -> list[int]:
     # Filter out and return the indices that remain True
     return [num for num, prime in enumerate(is_prime) if prime]
 
-def erato2(n: int) -> list[bool]:
-
-    """Returns a list of all prime numbers up to n."""
-    if n < 2:
-        return []
-
-    # Initialize a boolean list tracking the prime status of numbers 0 to n
-    is_prime = [True] * (n + 1)
-    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime
-
-    # Only iterate up to the square root of n
-    for p in range(2, int(n**0.5) + 1):
-        if is_prime[p]:
-            # Mark all multiples of p starting from p*p as false
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-
-    # Filter out and return the indices that remain True
-    # return [num for num, prime in enumerate(is_prime) if prime]
-    return is_prime
-
 # def esprimo(n):
 #     i = 2
 #     cd = 0
@@ -76,6 +55,12 @@ def esprimoV2(n):
              return False
     return True
 
+def esprimoV2_1(n):
+    for i in range(2,int(n**0.5)):
+        if divmod(n, i) == 0:
+             return False
+    return True
+
 def esprimoV3(n: int, primes: list[int]):
     for i in primes:
         if (i > int(n**0.5)):
@@ -84,8 +69,6 @@ def esprimoV3(n: int, primes: list[int]):
              return False
     return True
 
-def esprimoV4(n: int, primes: list[bool]):
-    return primes[n]
 
 # def prog():
 #     t1 = time.time()
@@ -106,17 +89,19 @@ def esprimoV4(n: int, primes: list[bool]):
 def prog():
     t1 = time.time()
     for i in range(11,1000000):
-        if esprimoV2(i) and esprimoV2(i+2) and esprimoV2(i+6) and esprimoV2(i+8):
+        if esprimoV2_1(i) and esprimoV2_1(i+2) and esprimoV2_1(i+6) and esprimoV2_1(i+8):
             print (i, i+2, i+6,i+8)
     t2 = time.time()
     times = t2 - t1
     print(times)
-    return times
+    # return times
 
 def prog1():
     t1 = time.time()
-    primes = erato(int(1000000))
-    for i in range(11,1000000, 10):
+    primes = erato(int(1000000**0.5))
+    print(11,13,17,19)
+    print(101, 103, 107,109)
+    for i in range(111,1000000, 10):
         if esprimoV3(i, primes) and esprimoV3(i+2, primes) and esprimoV3(i+6, primes) and esprimoV3(i+8, primes):
             print (i, i+2, i+6,i+8)
     t2 = time.time()
@@ -124,27 +109,6 @@ def prog1():
     print(times)
     return times
 
-def prog2():
-    t1 = time.time()
-    primes = erato2(int(1000000))
-    for i in range(11,1000000):
-        if esprimoV4(i, primes) and esprimoV4(i+2, primes) and esprimoV4(i+6, primes) and esprimoV4(i+8, primes):
-            print (i, i+2, i+6,i+8)
-    t2 = time.time()
-    times = t2 - t1
-    print(times)
-    return times
-
-def prog3():
-    t1 = time.time()
-    primes = erato2(int(1000000))
-    for i in range(11,1000000, 10):
-        if esprimoV4(i, primes) and esprimoV4(i+2, primes) and esprimoV4(i+6, primes) and esprimoV4(i+8, primes):
-            print (i, i+2, i+6,i+8)
-    t2 = time.time()
-    times = t2 - t1
-    print(times)
-    return times
 
 # p = prog()
 # print('-------------------------')
